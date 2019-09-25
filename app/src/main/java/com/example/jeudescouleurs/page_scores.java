a2594db1c359c844936b5e354802577e47ece775
mbogusz <marie.bogusz@soprasteria.com>
2019-09-25 11:21:21 +0200

    commit

---
+++ app/src/main/java/com/example/jeudescouleurs/page_scores.java
@@ -0,0 +1,13 @@
+package com.example.jeudescouleurs;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class page_scores extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.page_scores);
+    }
+}
